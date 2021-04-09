fbe279ffdd1f7648bb3515376916bfd1da89617c
ludo <ludovic.bedard@gmail.com>
2021-04-09 13:48:09 -0400

    LB - Added the XOR harness.

---
+++ SpeciesManager.py
@@ -0,0 +1,6 @@
+class SpeciesManager:
+    def __init__(self):
+        print('instanciated species manager')
+
+    
+    
---
+++ test_xor.py
@@ -0,0 +1,6 @@
+from NeatPipeline import neat_pipeline
+from XOREvaluator import XOREvaluator
+
+if __name__ == "__main__":
+    evaluator = XOREvaluator()
+    neat_pipeline(30, 3, 1, evaluator, "TestResults/", 30, 0)
