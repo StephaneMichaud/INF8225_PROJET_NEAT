cd9d9d89c86f41be74fe03e97e25434bad427b84
Stephame MIchaud <michaudstephane@yahoo.ca>
2021-04-11 10:20:28 -0400

    SM - Modified Neat pipeline to take into account recent progress

--- XOREvaluator.py
+++ XOREvaluator.py
@@ -6,10 +6,10 @@ class XOREvaluator:
         '''Evaluates the current population'''
 
         for genome in current_population:
-            fitness = 0
+            fitness = 1
             for a in range(len(self.xor)):
                 for b in range(len(self.xor[a])):
                     prediction = genome.feed_forward([1,a,b]) # the 1 is the bias node
-                    fitness += abs(prediction-self.xor[a][b])/(len(self.xor)*len(self.xor[a]))
+                    fitness -= abs(prediction-self.xor[a][b])/(len(self.xor)*len(self.xor[a]))
             
             genome.fitness = fitness
