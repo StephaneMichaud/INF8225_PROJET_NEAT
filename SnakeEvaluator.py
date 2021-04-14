8ad704069107d211d42b8c2ceb64de2d0d698afd
ludo <ludovic.bedard@gmail.com>
2021-04-14 15:44:49 -0400

    LB - Brain dead snake yay

--- SnakeEvaluator.py
+++ SnakeEvaluator.py
@@ -2,10 +2,15 @@ import gym
 import gym_snake
 from functools import reduce
 
-
+# https://github.com/grantsrb/Gym-Snake
 class SnakeEvaluator:
     def __init__(self):
         self.env = gym.make('snake-v0')
+        self.initialize_attributes()
+        
+
+    def initialize_attributes(self):
+        self.env.reset() # Constructs an instance of the game
         self.game_controller = self.env.controller
         self.snake = self.game_controller.snakes[0]
         self.BODY_COLOR = self.game_controller.grid.BODY_COLOR
@@ -17,13 +22,14 @@ class SnakeEvaluator:
         
         self.CONTROL_RANGES = [i/(float(self.NB_ACTIONS)-1) for i in range(0, self.NB_ACTIONS+1)]
 
+
     def get_nb_inputs_nn(self):
         return self.game_controller.grid.grid_size[0]*self.game_controller.grid.grid_size[1] + 1
 
     def convert_nn_output_to_action(self, nn_output):
         nn_output = nn_output[0]
         for i in range(0, self.NB_ACTIONS):
-            if nn_output > self.CONTROL_RANGES[i] and nn_output < self.CONTROL_RANGES[i+1]:
+            if nn_output >= self.CONTROL_RANGES[i] and nn_output < self.CONTROL_RANGES[i+1]:
                 return self.ACTIONS[i]
 
         raise Exception()
@@ -39,7 +45,7 @@ class SnakeEvaluator:
         
         for x in range(0, self.env.unit_size):
             for y in range(0, self.env.unit_size):
-                current_pixel_color = grid_object.grid.grid_pixels[x+offset_x, y+offset_y]
+                current_pixel_color = grid_object.grid[x+offset_x, y+offset_y]
                 if self.equal_colors(self.BODY_COLOR, current_pixel_color):
                     return 2
                 elif self.equal_colors(self.HEAD_COLOR, current_pixel_color):
@@ -65,35 +71,35 @@ class SnakeEvaluator:
         return nn_input
 
 
-
-    def snake_alive(self):
-        return self.snake != None
-
-
     def get_snake_default_fitness(self):
         return self.game_controller.grid.grid_size[0]*self.game_controller.grid.grid_size[1]
 
     
     def adjust_fitness(self, genome, reward, total_time_steps):
-        genome += self.get_snake_default_fitness()*reward
-        genome -= 1 # penalty for time passing
+        genome.fitness += self.get_snake_default_fitness()*reward
+        genome.fitness -= 1 # penalty for time passing
 
 
     def evaluate_genomes(self, current_population):
         '''Evaluates the current population'''
 
         for genome in current_population:
-            observation = self.env.reset() # Constructs an instance of the game
+            self.initialize_attributes()
             genome.fitness = self.get_snake_default_fitness()
             total_time_steps = 0
-            while self.snake_alive():
+            is_snake_alive = True
+            while is_snake_alive:
                 nn_input = self.convert_grid_to_input(self.game_controller.grid)
                 nn_output = genome.feed_forward(nn_input)
                 action = self.convert_nn_output_to_action(nn_output)
 
-                reward = observation.step(action)
+                state = self.env.step(action)
+                self.env.render()
+                reward = state[1] # index for reward
+                
                 self.adjust_fitness(genome, reward, total_time_steps)
-
+                
+                is_snake_alive = state[3]['snakes_remaining'] == 1
                 total_time_steps+=1
             
 
